9e39b508ae4629148f2b40f80dff0e0415962659
bstascavage <brian@stascavage.com>
2022-07-11 03:06:22 +0000

    Disabling amplify in frontend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// import { Amplify } from 'aws-amplify';
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 const header = ReactDOM.createRoot(document.getElementById("root"));
 header.render(
