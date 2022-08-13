bbf363b6a05f7f1da713c458743bb8f2dc4ec549
bstascavage <brian@stascavage.com>
2022-08-13 21:40:53 +0000

    Adding plausible support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import Plausible from "plausible-tracker";
+
+const { enableAutoPageviews } = Plausible({
+  domain: "https://statscollide.com",
+});
+enableAutoPageviews();
+
 const header = ReactDOM.createRoot(document.getElementById("root"));
 header.render(
   <React.StrictMode>
