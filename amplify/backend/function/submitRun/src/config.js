b2e898bbc547981f5df9e0a7e3df7cc9cc51620b
Brian <brian@stascavage.com>
2023-02-10 14:22:25 -0500

    Adding Ultros Season 3 (#10)

--- amplify/backend/function/submitRun/src/config.js
+++ amplify/backend/function/submitRun/src/config.js
@@ -14,6 +14,20 @@ const config = {
       },
       excludedAbilities: ["Possess", "Shock", "Magitek"],
     },
+    Ultros_League__Season_3: {
+      startingChars: 3,
+      minChars: 6,
+      minEspers: 9,
+      skip: {
+        reqs: {
+          chars: 9,
+          espers: 12,
+        },
+        conditionsMet: 1,
+        operation: "OR",
+      },
+      excludedAbilities: ["Possess", "Shock"],
+    },
     Coliseum__Terra: {
       startingChars: 3,
       minChars: 6,
