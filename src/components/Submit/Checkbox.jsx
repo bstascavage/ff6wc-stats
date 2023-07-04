cf0a8460ca8d3cccb913992c40d50ed5ce1be420
Brian <brian@stascavage.com>
2023-07-04 11:54:21 -0400

    Adding Statscompanion file upload for run submission
    * Adding checkbox valueSetter onHandler
    
    * Adding toggle valueSetter onHandler
    
    * Adding dropdown valueSetter onHandler
    
    * Adding textinput valueSetter onHandler
    
    * Fixing hour check in checkTime
    
    * statscompanion file upload
    
    * Fixing typo with pull request template

--- src/components/Submit/Checkbox.jsx
+++ src/components/Submit/Checkbox.jsx
@@ -4,20 +4,38 @@ import "./scss/main.scss";
 function CheckBox(props) {
   return (
     <fieldset>
-      <div className="submission-checkbox">
-        {renderCheckbox(props.renderList)}
-      </div>
+      <div className="submission-checkbox">{renderCheckbox(props)}</div>
     </fieldset>
   );
 }
 
-function renderCheckbox(elems) {
+function renderCheckbox(props) {
+  const handleChange = (value) => {
+    let selectionList = props.value.enum[props.id];
+
+    for (let i = 0; i < selectionList.length; i++) {
+      if (selectionList[i].name === value.target.name) {
+        selectionList[i].checked = value.target.checked;
+      }
+    }
+    props.value.enum[props.id] = selectionList;
+    props.valueSetter({ ...props.value });
+  };
+
   // Populate checkbox selection
   let renderList = [];
+  const elems = props.value.enum[props.id];
+
   for (let i = 0; i < elems.length; i++) {
     renderList.push(
       <div className="submission-checkbox-elem" key={elems[i].name}>
-        <input type="checkbox" id={elems[i].name} name={elems[i].name}></input>
+        <input
+          type="checkbox"
+          onChange={handleChange}
+          id={elems[i].name}
+          name={elems[i].name}
+          checked={elems[i].checked}
+        ></input>
         <label htmlFor={elems[i].name}>{elems[i].display_name}</label>
       </div>,
     );
